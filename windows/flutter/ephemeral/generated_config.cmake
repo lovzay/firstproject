5b512494146399a1100ba631e7e1b02f5f1b4e29
lovzay <tfvin24@gmail.com>
2023-11-24 15:56:27 +0800

    initial commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\DEV\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\DEV\\flutter projects\\firstproject" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\DEV\\flutter"
+  "PROJECT_DIR=D:\\DEV\\flutter projects\\firstproject"
+  "FLUTTER_ROOT=D:\\DEV\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\DEV\\flutter projects\\firstproject\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\DEV\\flutter projects\\firstproject"
+  "FLUTTER_TARGET=D:\\DEV\\flutter projects\\firstproject\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNTQ1Zjg3MDVkZjMwMTg3N2Q3ODcxMDdiYWMxYTZlOWZjOWVlMWFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\DEV\\flutter projects\\firstproject\\.dart_tool\\package_config.json"
+)
